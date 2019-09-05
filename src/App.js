b4285a7cdfb3c33cec4fb8de412f2c784409dbd9
2Re-play <wogusduddnjs@gmail.com>
2019-09-05 15:34:44 +0900

    componets event

--- src/App.js
+++ src/App.js
@@ -8,20 +8,44 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { // state 초기화
-            subject : {title: 'WEB', sub: "World Wide Web!"}
+            mode: "read",
+            subject : {title: 'WEB', sub: "World Wide Web!"},
+            welcome: {title: "welcome", desc : "Hello, React!!"},
+            contents : [
+                {id:1, title: 'HTML', desc: 'Html is for information '},
+                {id:2, title: 'CSS', desc: 'CSS is for information '},
+            ]
         }
     }
-  render() {
+    // state의 값이 바뀌면 render()함수가 다시 호출되며 하위에 컴포넌트도 다시 호출된다.
+    render() {
+        console.log('APP render');
+        let _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
+        console.log('render', this);
       return (
           <div className="App">
+              {/*state를 바꾸는 방법 setState(), bind로 this를 묶어준다.*/}
               <Subject
                   title = {this.state.subject.title}
-                  sub = {this.state.subject.sub}/>
-              <TOC/>
-              <Content title = "HTML" desc = "HTML is HyperText Markup Language."/>
+                  sub = {this.state.subject.sub}
+                  onChangePage = {function(){
+                      this.setState({mode : "welcome"})
+                  }.bind(this)}
+              />
+              <TOC
+              data={this.state.contents}
+              />
+              <Content title = {_title} desc = {_desc}/>
           </div>
       );
-  }
+    }
 }
 
 export default App;
--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import React from "react"
 
 class Content extends Component{
     render() {
+        console.log('content render')
         return (
             <article>
                 <h2>{this.props.title}</h2>
