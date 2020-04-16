58c5bb8ff8f0e63f9e68ce847b216da51012d317
2Re-play <wogusduddnjs@gmail.com>
2020-04-16 20:10:54 +0900

    ErrorBoundary 컴포넌트 추가

--- react-tutorial/src/ErrorBoundary.js
+++ react-tutorial/src/ErrorBoundary.js
@@ -15,9 +15,9 @@ class ErrorBoundary extends Component {
         this.setState({
             error: true
         });
-        if (process.env.NODE_ENV === 'production') {
-            Sentry.captureException(error, { extra: info });
-        }
+        // if (process.env.NODE_ENV === 'production') {
+        //     Sentry.captureException(error, { extra: info });
+        // }
     }
 
     render() {
