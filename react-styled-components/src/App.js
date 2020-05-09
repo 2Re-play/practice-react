127bbd4f302fb01bcf68881b223a69f02132b469
2Re-play <wogusduddnjs@gmail.com>
2020-05-09 17:35:29 +0900

    styled components

---
+++ react-styled-components/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TotalBackground = styled.button`
+fon
+`
+
+function App() {
+  return (
+    <div className="App">
+      <button>hello</button>
+
+    </div>
+  );
+}
+
+export default App;
